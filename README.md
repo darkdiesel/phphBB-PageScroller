ed61ae23839563f54d7ca057a898a6e8d67ff9e2
Igor Peshkov <igor.peshkov@gmail.com>
2016-01-31 21:52:00 +0300

    Release 1.0.0 version of page scroller extension. Add ability to scroll page.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# phphBB-PageScroller
+# phpBB 3.1 Extension - Page Scroller
+
+## About
+
+Page scroller extension add ability to scroll page to the top of page or to the bottom for phpBB 3.1
+
+## Installation
+
+Clone into phpBB folder /ext/dark_diesel/page_scroller:
+
+    git clone git@github.com:darkdiesel/phphBB-PageScroller.git phpBB folder /ext/dark_diesel/page_scroller
+
+Go to "ACP" > "Customise" > "Extensions" and enable the "Page Scroller" extension.
+
+## License
+
+[GPLv2](license.txt)
---
+++ ext.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * This file is written by Igor Peshkov.
+ *
+ * @package phpBB Extension - Page Scroller
+ * @copyright (c) Igor Peshkov (dark_diesel) <https://plus.google.com/+IgorPeshkov>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, contact with developer
+ *
+ */
+
+
+namespace dark_diesel\page_scroller;
+
+/**
+* Extension class for custom enable/disable/purge actions
+*/
+class ext extends \phpbb\extension\base
+{
+	/** @var string Require 3.1.2 due to updated INCLUDECSS syntax */
+	const PHPBB_VERSION = '3.1.2';
+
+	/**
+	 * Enable extension if phpBB minimum version requirement is met
+	 *
+	 * @return bool
+	 */
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return phpbb_version_compare($config['version'], self::PHPBB_VERSION, '>=');
+	}
+}
