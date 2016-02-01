9f7b2f6de53c08a5363520ebd81fa8bcd2d2bf37
Peshkov, Igor <i.peshkov@itransition.com>
2016-02-01 14:51:13 +0300

    #2 - Update page scroller position via css classes.

--- styles/all/theme/assets/scripts/page-scroller.js
+++ styles/all/theme/assets/scripts/page-scroller.js
@@ -1,71 +1,58 @@
-(function($) {
-	$.fn.pageScroller = function(options) {
-		var settings = $.extend({
-			'position': 'left-middle',
-			'block-visible': '30',
-			'animation-speed-out': 500,
-			'animation-speed-in': 200,
-		}, options);
+(function ($) {
+    $.fn.pageScroller = function (options) {
+        var settings = $.extend({
+            'position': 'left-middle',
+            'block-visible': '30',
+            'animation-speed-out': 500,
+            'animation-speed-in': 200,
+        }, options);
 
-		return this.each(function() {
-			$(this).css({
-				'position': 'fixed'
-			});
+        return this.each(function () {
+            $(this).css({
+                'position': 'fixed'
+            });
 
-			switch (settings['position']) {
-				case 'left-top':
-					$(this).css({
-						'top': '0', 'left': 0
-					});
-					break
-				case 'left-middle':
-					$(this).css({
-						'top': '50%', 'left': 0, 'margin-top': -($(this).height() / 2)
-					}).animate({'left': settings['block-visible'] - $(this).outerWidth()}, settings['animation-speed-out']).hover(
-							function() {
-								$(this).stop().animate({'left': 0}, settings['animation-speed-in']);
-							}, function() {
-								$(this).stop().animate({'left': settings['block-visible'] - $(this).outerWidth()}, settings['animation-speed-out']);
-						}
-							);
-					break
-				case 'left-bottom':
-					$(this).css({
-						'bottom': '0', 'left': 0
-					});
-					break
-				case 'right-top':
-					$(this).css({
-						'top': '0', 'right': 0
-					});
-					break
-				case 'right-middle':
-					$(this).css({
-						'top': '50%', 'right': 0, 'margin-top': -($(this).height() / 2)
-					});
-					break
-				case 'right-bottom':
-					$(this).css({
-						'bottom': '50px', 'right': '25px'
-					});
-					break
-			}
+            switch (settings['position']) {
+                case 'right-top':
+                    $(this).addClass('right-top');
+                    break;
 
-			$(this).on("click", ".page-scroller-up-btn", function(e) {
-				$('html,body').animate({
-					scrollTop: 0
-				}, 800);
-				e.preventDefault();
-			}).on("click", ".page-scroller-down-btn", function(e) {
-				$('html,body').animate({
-					scrollTop: $(document).outerHeight()
-				}, 800);
-				e.preventDefault();
-			});
-		});
+                case 'left-top':
+                    $(this).addClass('left-top');
+                    break;
 
-		function hoverLeft() {
+                case 'right-middle':
+                    $(this).addClass('right-middle');
+                    break;
 
-		}
-	};
+                case 'left-middle':
+                    $(this).addClass('left-middle');
+                    break;
+
+                case 'right-bottom':
+                    $(this).addClass('right-bottom');
+                    break;
+
+                case 'left-bottom':
+                    $(this).addClass('left-bottom');
+                    break;
+            }
+
+            $(this).on("click", ".page-scroller-up-btn", function (e) {
+                $('html,body').animate({
+                    scrollTop: 0
+                }, 800);
+                e.preventDefault();
+            }).on("click", ".page-scroller-down-btn", function (e) {
+                $('html,body').animate({
+                    scrollTop: $(document).outerHeight()
+                }, 800);
+                e.preventDefault();
+            });
+        });
+
+        function hoverLeft() {
+
+        }
+    };
 })(jQuery);
